4f66fe2cdcf211ed1cfa43744474395b7eeaf50f
André Baldeweg <a@baldeweg.org>
2022-09-20 17:54:28 +0000

    composition api for i18n

--- src/i18n.js
+++ src/i18n.js
@@ -5,6 +5,7 @@ import de_DE from './../locales/de-DE.json'
 export default createI18n({
   locale: import.meta.env.VUE_APP_I18N_LOCALE,
   fallbackLocale: 'en-US',
+  allowComposition: true,
   messages: {
     'en-US': en_US,
     'de-DE': de_DE,
